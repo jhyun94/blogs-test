9d7651ba043a4823ad6417962df7f1dfe343deaf
Jonathan Hyun <email": null>
2017-02-13 08:17:39 -0600

    works

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  get 'welcome/index'
+
+  root 'welcome#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :articles
+    resources :comments
+
+  root 'welcome#index'
+
+
+end
+
+
